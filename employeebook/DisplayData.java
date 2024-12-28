41fe49135ee82a3b330e995b5eae38e5c8bf9f2d
Rudraksha Kokane <rudrark0109@gmail.com>
2024-12-27 21:36:57 -0600

    Removing senseless comments

--- employeebook/DisplayData.java
+++ employeebook/DisplayData.java
@@ -1,7 +1,7 @@
 package employeebook;
 
-import java.awt.event.ActionEvent;          //import actionEvent
-import java.awt.event.ActionListener;       //import actionListener for ActionEvent
+import java.awt.event.ActionEvent;          
+import java.awt.event.ActionListener;       
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
